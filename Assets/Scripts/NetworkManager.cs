145e8b905fc60967bb4a7cf0631793cc681a08aa
J0SE-S <joseph.simutis@icloud.com>
2023-03-18 16:16:27 -0400

    Added Server-Client Connection

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Riptide;
+using Riptide.Utils;
 
-public class NetworkManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class NetworkManager : MonoBehaviour {
+    public enum ServerToClientId : ushort {
+        Movement = 0
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public enum ClientToServerId : ushort {
+        AdjustPosition = 100
+    }
+
+    private static NetworkManager _singleton;
+    public static NetworkManager Singleton {
+        get => _singleton;
+        private set {
+            if (_singleton == null)
+                _singleton = value;
+            else if (_singleton != value) {
+                Debug.Log($"{nameof(NetworkManager)} instance already exists, destroying object!");
+                Destroy(value);
+            }
+        }
+    }
+
+    internal Server Server { get; private set; }
+    internal Client Client { get; private set; }
+
+    [SerializeField] private string ip;
+    [SerializeField] private ushort port;
+    [SerializeField] private ushort maxPlayers;
+
+    void Start() {
+        RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
+        Server = new Server();
+        Client = new Client();
+    }
+
+    private void FixedUpdate() {
+        if (Server.IsRunning)
+            Server.Update();    
+        Client.Update();
+    }
+
+    private void StartHost() {
+        Server.Start(port, maxPlayers);
+    }
+
+    private void JoinGame() {
+        Client.Connect($"{ip}:{port}");
+    }
+
+    private void OnApplicationQuit() {
+        Server.Stop();
+        Client.Disconnect();
     }
 }
