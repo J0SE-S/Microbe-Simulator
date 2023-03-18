4e563565ed1a188e7a95181838420fa81bb2eede
J0SE-S <joseph.simutis@icloud.com>
2023-03-18 09:38:21 -0400

    Finished work on title screen, imported network libraries

--- Assets/Scripts/LoadingScreenScript.cs
+++ Assets/Scripts/LoadingScreenScript.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadingScreenScript : MonoBehaviour {
+    public static LoadingScreenScript Instance;
     public string sceneToLoad;
 
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     void Start() {
-        SceneManager.LoadScene(sceneToLoad);
+        Instance = this;
+        DontDestroyOnLoad(this);
+        if (SceneManager.GetActiveScene().name == "LoadingScreen") {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    void OnSceneLoad() {
+        if (SceneManager.GetActiveScene().name == "LoadingScreen") {
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
--- Assets/Scripts/TitleScreenScript.cs
+++ Assets/Scripts/TitleScreenScript.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleScreenScript : MonoBehaviour {
-    // Start is called before the first frame update
+    public GameObject MainMenuCanvas;
+    public GameObject SettingsCanvas;
+
     public void PlayGame() {
         //SceneManager.LoadScene("GameScene");
     }
 
     // Update is called once per frame
     public void OpenSettingsMenu() {
+        MainMenuCanvas.SetActive(false);
+        SettingsCanvas.SetActive(true);
     }
 
     public void QuitGame() {
         Application.Quit(0);
+    }
+
+    public void BackToMainMenu() {
+        MainMenuCanvas.SetActive(true);
+        SettingsCanvas.SetActive(false);
     }
 }
